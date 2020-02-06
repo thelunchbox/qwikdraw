eb34dcf76bfff3d5bbe2d83ef3ce43c0a28c8309
Alex Merkert <alex.merkert@gmail.com>
2020-02-06 14:40:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -28,8 +28,21 @@ window.addEventListener('load', e => {
     const context = canvas.getContext('2d');
     
     let fp = null;
-    
     const qd = {};
+    const IMAGE_CACHE = {};
+    
+    const loadImage = ({name, path}) => new Promise(resolve => {
+        const i = new Image();
+        i.src = path;
+        i.onload = () => {
+            IMAGE_CACHE[name] = i;
+            resolve(true);
+        };
+        i.onerror = () => {
+            IMAGE_CACHE[name] = '[error]';
+            resolve(false);
+        };
+    });
     
     const __initialize = operation => {
         fp = null;
@@ -40,7 +53,11 @@ window.addEventListener('load', e => {
             console.error('Error while drawing:', ex);
         }
         canvas.restore();
+        
+        this.loadImages = images => Promise.all(images.map(img => loadImage(img));
     };
+                                                
+    // await qd.loadImages([{ name: 'hi', path: 'hi.png' }]);
     
     // we want to allow draws to be nested inside other draws
     qd.nest = __initialize;
@@ -54,8 +71,18 @@ window.addEventListener('load', e => {
     };
     
     qd.close = () => {
-        const [x, y] = fp;
-        context.lineTo(x, y);
+        if (fp) {
+            const [x, y] = fp;
+            context.lineTo(x, y);
+        }
+        return qd;
+    };
+    
+    qd.vector = (x, y, r, angle) => {
+        context.moveTo(x, y);
+        const x1 = r * Math.cos(angle) + x;
+        const y1 = r * Math.sin(angle) + y;
+        context.lineTo(x1, y1);
         return qd;
     };
     
@@ -81,9 +108,10 @@ window.addEventListener('load', e => {
         return qd;
     };
     
-    qd.stroke = (color, { width = 1 } = {}) => {
+    qd.stroke = (color, { width = 1, dashPattern = [] } = {}) => {
         context.strokeStyle = color;
         context.lineWidth = width;
+        context.setLineDash(dashPattern);
         context.stroke();
         return qd;
     };
